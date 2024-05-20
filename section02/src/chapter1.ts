0340e5c73b8435b37a8223d411e436daf8cd54b6
subin114 <bin7851@naver.com>
2024-05-20 13:25:03 +0900

    원시타입과 리터럴타입

---
+++ section02/src/chapter1.ts
@@ -0,0 +1,32 @@
+// number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+// string
+let str1: string = "hello";
+let str2: string = `hello`;
+let str3: string = `hello ${num1}`;
+
+// boolean
+let boo1: boolean = true;
+let boo2: boolean = false;
+
+// null
+let null1: null = null;
+
+// undefined
+let unde1: undefined = undefined;
+
+// let numA: number = null;
+// "strictNullChecks": false로 설정하면 에러 X (하지만 true로 설정하는게 안전함)
+
+// 리터럴 타입
+let numA: 10 = 10;
+// numA = 12; // error
+let strA: "hello" = "hello";
+let boolA: true = true;
