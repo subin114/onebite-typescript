cbab815d78e34142d66ddde6011e8878aad3525e
subin114 <bin7851@naver.com>
2024-05-30 23:06:55 +0900

    infer - 조건부 타입 내에서 타입 추론

---
+++ section09/src/chapter2.ts
@@ -0,0 +1,27 @@
+/**
+ * infer
+ * - 조건부 타입 내에서 특정 타입만 딱 추론해 올 수 있는 기능
+ */
+
+type FuncA = () => string;
+type FuncB = () => number;
+
+type ReturnType<T> = T extends () => infer R ? R : never;
+
+type A = ReturnType<FuncA>;
+type B = ReturnType<FuncB>;
+type C = ReturnType<number>; // 조건식이 거짓이 되기 때문에 추론 불가 (never)
+
+/**
+ * 예제
+ */
+
+type PromiseUnpack<T> = T extends Promise<infer R> ? R : never;
+// 1. T는 프로미스 타입이어야 한다.
+// 2. 프로미스 타입의 결과값 타입을 반환해야 한다.
+
+type PromiseA = PromiseUnpack<Promise<number>>;
+// number
+
+type PromiseB = PromiseUnpack<Promise<string>>;
+// string
